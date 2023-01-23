eeb7bb1c2617aee82aa6cebaad3b5ca2586a4969
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2023-01-23 07:46:41 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app     = express();
+const PORT    = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('./index');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
+});
